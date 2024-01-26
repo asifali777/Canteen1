92a2aa81ea0c7f60de4bcd26d7d25d1b88216784
asifali777 <asifali45007@gmail.com>
2024-01-26 19:21:32 +0530

    commit7

--- build/script.js
+++ build/script.js
@@ -1,8 +1,10 @@
 let a = 0;
 // for item 1 
 document.getElementById("plus1").addEventListener("click",()=>{
+    if (a<4){
     a++;
     document.getElementById("num1").innerHTML = a;
+    }
 })
 document.getElementById("minus1").addEventListener("click",()=>{
     if (a>0) {
@@ -12,11 +14,14 @@ document.getElementById("minus1").addEventListener("click",()=>{
         return 0;
     }
 });
+
 let b = 0;
 // item2 
 document.getElementById("plus2").addEventListener("click",()=>{
-    b++;
+    if (b<4){
+        b++;
     document.getElementById("num2").innerHTML = b;
+    }
 })
 document.getElementById("minus2").addEventListener("click",()=>{
     if (b>0) {
@@ -26,11 +31,13 @@ document.getElementById("minus2").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item 3
 let c = 0;
 document.getElementById("plus3").addEventListener("click",()=>{
-    c++;
+    if (c<4) {
+        c++;
     document.getElementById("num3").innerHTML = c;
+    }
 })
 document.getElementById("minus3").addEventListener("click",()=>{
     if (c>0) {
@@ -40,11 +47,13 @@ document.getElementById("minus3").addEventListener("click",()=>{
         return 0;
     }
 })
-
+// item4
 let e = 0;
 document.getElementById("plus4").addEventListener("click",()=>{
+   if(e<4){
     e++;
     document.getElementById("num4").innerHTML = e;
+   }
 })
 document.getElementById("minus4").addEventListener("click",()=>{
     if (e>0) {
@@ -54,11 +63,13 @@ document.getElementById("minus4").addEventListener("click",()=>{
         return 0;
     }
 })
-
+// item5
 let f = 0;
 document.getElementById("plus5").addEventListener("click",()=>{
-    f++;
-    document.getElementById("num5").innerHTML = f;
+    if (f<4){
+        f++;
+        document.getElementById("num5").innerHTML = f;
+    }
 })
 document.getElementById("minus5").addEventListener("click",()=>{
     if (f>0) {
@@ -68,11 +79,13 @@ document.getElementById("minus5").addEventListener("click",()=>{
         return 0;
     }
 });
-    
+//   item6  
 let g = 0;
 document.getElementById("plus6").addEventListener("click",()=>{
-    g++;
+    if (g<4) {
+        g++;
     document.getElementById("num6").innerHTML = g;
+    }
 })
 document.getElementById("minus6").addEventListener("click",()=>{
     if (g>0) {
@@ -82,11 +95,13 @@ document.getElementById("minus6").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item7
 let h = 0;
 document.getElementById("plus7").addEventListener("click",()=>{
-    h++;
+    if (h<4) {
+        h++;
     document.getElementById("num7").innerHTML = h;
+    }
 })
 document.getElementById("minus7").addEventListener("click",()=>{
     if (h>0) {
@@ -96,11 +111,13 @@ document.getElementById("minus7").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item8
 let i = 0;
 document.getElementById("plus8").addEventListener("click",()=>{
-    i++;
+    if (i<4){
+        i++;
     document.getElementById("num8").innerHTML = i;
+    }
 })
 document.getElementById("minus8").addEventListener("click",()=>{
     if (i>0) {
@@ -110,11 +127,13 @@ document.getElementById("minus8").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item9
 let j =0;
 document.getElementById("plus9").addEventListener("click",()=>{
-    j++;
+    if (j<4) {
+        j++;
     document.getElementById("num9").innerHTML = j;
+    }
 })
 document.getElementById("minus9").addEventListener("click",()=>{
     if (j>0) {
@@ -124,11 +143,13 @@ document.getElementById("minus9").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item10
 let k = 0;
 document.getElementById("plus10").addEventListener("click",()=>{
-    k++;
+    if (k<4) {
+        k++;
     document.getElementById("num10").innerHTML = k;
+    }
 })
 document.getElementById("minus10").addEventListener("click",()=>{
     if (k>0) {
@@ -138,11 +159,13 @@ document.getElementById("minus10").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item11
 let l = 0;
 document.getElementById("plus11").addEventListener("click",()=>{
-    l++;
+    if (l<4){
+        l++;
     document.getElementById("num11").innerHTML = l;
+    }
 })
 document.getElementById("minus11").addEventListener("click",()=>{
     if (l>0) {
@@ -152,11 +175,13 @@ document.getElementById("minus11").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item12
 let m = 0;
 document.getElementById("plus12").addEventListener("click",()=>{
-    m++;
+    if (m<4) {
+        m++;
     document.getElementById("num12").innerHTML = m;
+    }
 })
 document.getElementById("minus12").addEventListener("click",()=>{
     if (m>0) {
@@ -166,11 +191,13 @@ document.getElementById("minus12").addEventListener("click",()=>{
         return 0;
     }
 });
-
+// item13
 let n = 0;
 document.getElementById("plus13").addEventListener("click",()=>{
-    n++;
+    if (n<4) {
+        n++;
     document.getElementById("num13").innerHTML = n;
+    }
 })
 document.getElementById("minus13").addEventListener("click",()=>{
     if (n>0) {
