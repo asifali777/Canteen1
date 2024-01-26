e1c0d08637693e5c81aeacacabd9341efad31f3a
asifali777 <asifali45007@gmail.com>
2024-01-26 21:30:52 +0530

    commit8

--- build/script.js
+++ build/script.js
@@ -1,35 +1,49 @@
 let a = 0;
 // for item 1 
 document.getElementById("plus1").addEventListener("click",()=>{
-    if (a<4){
-    a++;
+    if (a<4) {
+        a++;
     document.getElementById("num1").innerHTML = a;
-    }
+    document.getElementById("item1").checked = true;
+    };
+
 })
 document.getElementById("minus1").addEventListener("click",()=>{
-    if (a>0) {
+  
+    if (a > 0) {
         a--;
         document.getElementById("num1").innerHTML = a;
-    } else {
+        if (a === 0) {
+            document.getElementById("item1").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 
 let b = 0;
 // item2 
 document.getElementById("plus2").addEventListener("click",()=>{
-    if (b<4){
+    if (b<4) {
         b++;
     document.getElementById("num2").innerHTML = b;
-    }
+    document.getElementById("item2").checked = true;
+    };
+
 })
 document.getElementById("minus2").addEventListener("click",()=>{
-    if (b>0) {
+  
+    if (b > 0) {
         b--;
         document.getElementById("num2").innerHTML = b;
-    } else {
+        if (b === 0) {
+            document.getElementById("item2").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item 3
 let c = 0;
@@ -37,47 +51,68 @@ document.getElementById("plus3").addEventListener("click",()=>{
     if (c<4) {
         c++;
     document.getElementById("num3").innerHTML = c;
-    }
+    document.getElementById("item3").checked = true;
+    };
+
 })
 document.getElementById("minus3").addEventListener("click",()=>{
-    if (c>0) {
+  
+    if (c > 0) {
         c--;
         document.getElementById("num3").innerHTML = c;
-    } else {
+        if (c === 0) {
+            document.getElementById("item3").checked = false;
+        }
+    }else{
         return 0;
     }
-})
+ 
+});
 // item4
 let e = 0;
 document.getElementById("plus4").addEventListener("click",()=>{
-   if(e<4){
-    e++;
+    if (e<4) {
+        e++;
     document.getElementById("num4").innerHTML = e;
-   }
+    document.getElementById("item4").checked = true;
+    };
+
 })
 document.getElementById("minus4").addEventListener("click",()=>{
-    if (e>0) {
+  
+    if (e > 0) {
         e--;
         document.getElementById("num4").innerHTML = e;
-    } else {
+        if (e === 0) {
+            document.getElementById("item4").checked = false;
+        }
+    }else{
         return 0;
     }
-})
+ 
+});
 // item5
 let f = 0;
 document.getElementById("plus5").addEventListener("click",()=>{
-    if (f<4){
+    if (f<4) {
         f++;
-        document.getElementById("num5").innerHTML = f;
-    }
+    document.getElementById("num5").innerHTML = f;
+    document.getElementById("item5").checked = true;
+    };
+
 })
 document.getElementById("minus5").addEventListener("click",()=>{
-    if (f>0) {
+  
+    if (f > 0) {
         f--;
         document.getElementById("num5").innerHTML = f;
-    } else {
+        if (f === 0) {
+            document.getElementById("item5").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 //   item6  
 let g = 0;
@@ -85,15 +120,22 @@ document.getElementById("plus6").addEventListener("click",()=>{
     if (g<4) {
         g++;
     document.getElementById("num6").innerHTML = g;
-    }
+    document.getElementById("item6").checked = true;
+    };
+
 })
 document.getElementById("minus6").addEventListener("click",()=>{
-    if (g>0) {
+  
+    if (g > 0) {
         g--;
         document.getElementById("num6").innerHTML = g;
-    } else {
+        if (g === 0) {
+            document.getElementById("item6").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item7
 let h = 0;
@@ -101,31 +143,45 @@ document.getElementById("plus7").addEventListener("click",()=>{
     if (h<4) {
         h++;
     document.getElementById("num7").innerHTML = h;
-    }
+    document.getElementById("item7").checked = true;
+    };
+
 })
 document.getElementById("minus7").addEventListener("click",()=>{
-    if (h>0) {
+  
+    if (h > 0) {
         h--;
         document.getElementById("num7").innerHTML = h;
-    } else {
+        if (h === 0) {
+            document.getElementById("item7").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item8
 let i = 0;
 document.getElementById("plus8").addEventListener("click",()=>{
-    if (i<4){
+    if (i<4) {
         i++;
     document.getElementById("num8").innerHTML = i;
-    }
+    document.getElementById("item8").checked = true;
+    };
+
 })
 document.getElementById("minus8").addEventListener("click",()=>{
-    if (i>0) {
+  
+    if (i > 0) {
         i--;
         document.getElementById("num8").innerHTML = i;
-    } else {
+        if (i === 0) {
+            document.getElementById("item8").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item9
 let j =0;
@@ -133,15 +189,22 @@ document.getElementById("plus9").addEventListener("click",()=>{
     if (j<4) {
         j++;
     document.getElementById("num9").innerHTML = j;
-    }
+    document.getElementById("item9").checked = true;
+    };
+
 })
 document.getElementById("minus9").addEventListener("click",()=>{
-    if (j>0) {
+  
+    if (j > 0) {
         j--;
         document.getElementById("num9").innerHTML = j;
-    } else {
+        if (j === 0) {
+            document.getElementById("item9").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item10
 let k = 0;
@@ -149,61 +212,108 @@ document.getElementById("plus10").addEventListener("click",()=>{
     if (k<4) {
         k++;
     document.getElementById("num10").innerHTML = k;
-    }
+    document.getElementById("item10").checked = true;
+    };
+
 })
 document.getElementById("minus10").addEventListener("click",()=>{
-    if (k>0) {
+  
+    if (k > 0) {
         k--;
         document.getElementById("num10").innerHTML = k;
-    } else {
+        if (k === 0) {
+            document.getElementById("item10").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item11
 let l = 0;
 document.getElementById("plus11").addEventListener("click",()=>{
-    if (l<4){
+    if (l<4) {
         l++;
     document.getElementById("num11").innerHTML = l;
-    }
+    document.getElementById("item11").checked = true;
+    };
+
 })
 document.getElementById("minus11").addEventListener("click",()=>{
-    if (l>0) {
+  
+    if (l > 0) {
         l--;
         document.getElementById("num11").innerHTML = l;
-    } else {
+        if (l === 0) {
+            document.getElementById("item11").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item12
 let m = 0;
+
 document.getElementById("plus12").addEventListener("click",()=>{
     if (m<4) {
         m++;
     document.getElementById("num12").innerHTML = m;
-    }
+    document.getElementById("item12").checked = true;
+    };
+
 })
 document.getElementById("minus12").addEventListener("click",()=>{
-    if (m>0) {
+  
+    if (m > 0) {
         m--;
         document.getElementById("num12").innerHTML = m;
-    } else {
+        if (m === 0) {
+            document.getElementById("item12").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
 });
 // item13
 let n = 0;
+// const radioButton= document.getElementById("item14");
 document.getElementById("plus13").addEventListener("click",()=>{
     if (n<4) {
         n++;
     document.getElementById("num13").innerHTML = n;
-    }
+    document.getElementById("item13").checked = true;
+    };
+
 })
 document.getElementById("minus13").addEventListener("click",()=>{
-    if (n>0) {
+  
+    if (n > 0) {
         n--;
         document.getElementById("num13").innerHTML = n;
-    } else {
+        if (n === 0) {
+            document.getElementById("item13").checked = false;
+        }
+    }else{
         return 0;
     }
+ 
+});
+// last button 
+let o = 0;
+document.getElementById("plusF").addEventListener("click",()=>{
+        o++;
+    document.getElementById("numF").innerHTML = o;
+
+})
+document.getElementById("minusF").addEventListener("click",()=>{
+  
+    if (o > 0) {
+        o--;
+        document.getElementById("numF").innerHTML = o;
+    }else{
+        return 0;
+    }
+ 
 });
